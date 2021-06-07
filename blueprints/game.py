31c9b959c37d17b412f3db33c3423909635008dd
BuzzardM <mfbuijse@avans.nl>
2021-06-07 21:52:26 +0200

    possibly good logic implementation

--- blueprints/game.py
+++ blueprints/game.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import current_user
 
+from models.answer import Answer
 from models.color import Color
 from models.database import db
 from models.game import MIN_POS, MAX_POS, Game
+from models.guess import Guess
 
 game = Blueprint('game', __name__)
 
@@ -34,7 +36,8 @@ def new():
             player_id=current_user.id,
             amount_of_positions=positions,
             duplicate_colors=duplicate_colors,
-            score=0
+            score=0,
+            current_turn=1
         )
 
         for color_id in color_ids:
@@ -67,4 +70,29 @@ def make_guess(game_id: int):
         if playable_game is None:
             return redirect(url_for('game.index'))
 
-        playable_game.guesses.append(Guess())
+        guess = request.form.get('guess')
+
+        new_guess = Guess(
+            game_id=playable_game.id,
+            turn=playable_game.current_turn,
+            guess=' '.join(guess)
+        )
+
+        perfect_guesses, good_guesses, incorrect_guesses = playable_game.check_guess(guess)
+
+        new_answer = Answer(
+            game_id=playable_game.id,
+            turn=playable_game.current_turn,
+            perfect_guesses=perfect_guesses,
+            good_guesses=good_guesses,
+            incorrect_guesses=incorrect_guesses
+        )
+
+        playable_game.guesses.append(new_guess)
+        playable_game.answers.append(new_answer)
+        playable_game.current_turn += 1
+
+        db.session.add(playable_game)
+        db.session.commit()
+
+        return redirect(url_for('game.get_game',  game_id=playable_game.id))
--- models/answer.py
+++ models/answer.py
@@ -1,8 +1,10 @@
 from models.database import db
 
 
-class Guess(db.Model):
+class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
     turn = db.Column(db.Integer)
-    guess = db.Column(db.String(500))
+    perfect_guesses = db.Column(db.Integer)
+    good_guesses = db.Column(db.Integer)
+    incorrect_guesses = db.Column(db.Integer)
